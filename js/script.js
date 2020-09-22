3be817b83ead40758b1d0653724f7a714fcda129
Auliz <joseph.auz77@gmail.com>
2020-09-21 23:13:18 -0500

    added objects and variables for game pieces

--- js/script.js
+++ js/script.js
@@ -2,8 +2,50 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d'); // ctx = context
 
+const netWidth = 4;
+const netHeight = canvas.height;
 
+const paddleWidth = 10;
+const paddleHeight = 100;
 
+let upArrowPressed = false;
+let downArrowPressed = false;
+
+const net = {
+  x: canvas.width / 2 - netWidth / 2,
+  y: 0,
+  width: netWidth,
+  height: netHeight,
+  color: '#FFF'
+}
+
+const user = {
+  x: 10,
+  y: canvas.height / 2 - paddleHeight / 2,
+  width: paddleWidth,
+  height: paddleHeight,
+  color: '#FFF',
+  score: 0
+}
+
+const ai = {
+  x: canvas.width - (paddleWidth + 10),
+  y: canvas.height / 2 - paddleHeight / 2,
+  width: paddleWidth,
+  height: paddleHeight,
+  color: '#FFF',
+  score: 0
+}
+
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  radius: 7,
+  speed: 7,
+  velocityX: 5,
+  velocityY: 5,
+  color: '#05EDFF'
+}
 
 
 render = () => {
