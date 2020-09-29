52b7306d1e2b3d5a73e601cfa0aa0a930544e4b1
Auliz <joseph.auz77@gmail.com>
2020-09-28 23:18:36 -0500

    v1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d'); // ctx = context
+
+const hitSound = new Audio('../sounds/hitSound.wav');
+const scoreSound = new Audio('../sounds/scoreSound.wav');
+const wallHitSound = new Audio('../sounds/wallHitSound.wav');
+hitSound.volume = 0.2;
+scoreSound.volume = 0.2;
+wallHitSound.volume = 0.2;
 
 const netWidth = 4;
 const netHeight = canvas.height;
@@ -16,8 +22,8 @@ const net = {
   y: 0,
   width: netWidth,
   height: netHeight,
-  color: '#FFF'
-}
+  color: '#FFF',
+};
 
 const user = {
   x: 10,
@@ -25,8 +31,8 @@ const user = {
   width: paddleWidth,
   height: paddleHeight,
   color: '#FFF',
-  score: 0
-}
+  score: 0,
+};
 
 const ai = {
   x: canvas.width - (paddleWidth + 10),
@@ -34,8 +40,8 @@ const ai = {
   width: paddleWidth,
   height: paddleHeight,
   color: '#FFF',
-  score: 0
-}
+  score: 0,
+};
 
 const ball = {
   x: canvas.width / 2,
@@ -44,21 +50,145 @@ const ball = {
   speed: 7,
   velocityX: 5,
   velocityY: 5,
-  color: '#05EDFF'
+  color: '#05EDFF',
+};
+
+function drawNet() {
+  ctx.fillStyle = net.color;
+  ctx.fillRect(net.x, net.y, net.width, net.height);
 }
 
-drawNet = () => {
-  ctx.fillStyle = net.color
-  ctx.fillRect(net.x, net.y, net.width, net.height)
+function drawScore(x, y, score) {
+  ctx.fillStyle = '#fff';
+  ctx.font = '35px sans-serif';
+  ctx.fillText(score, x, y);
 }
 
-
-
-render = () => {
-  ctx.fillStyle = '#000'
-  ctx.fillRect(0,0, canvas.width, canvas.height)
-
-  drawNet()
+function drawPaddle(x, y, width, height, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
 }
 
-render();
+function drawBall(x, y, radius, color) {
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2, true); // π * 2 Radians = 360 degrees
+  ctx.closePath();
+  ctx.fill();
+}
+
+window.addEventListener('keydown', keydownHandler);
+window.addEventListener('keyup', keyUpHandler);
+
+function keydownHandler(event) {
+  switch (event.keyCode) {
+    case 38:
+      upArrowPressed = true;
+      break;
+    case 40:
+      downArrowPressed = true;
+      break;
+  }
+}
+
+function keyUpHandler(event) {
+  switch (event.keyCode) {
+    case 38:
+      upArrowPressed = false;
+      break;
+  
+    case 40:
+      downArrowPressed = false;
+      break;
+  }
+}
+
+function reset() {
+  ball.x = canvas.width / 2;
+  ball.y = canvas.height / 2;
+  ball.speed = 7;
+
+  ball.velocityX = -ball.velocityX;
+  ball.velocityY = -ball.velocityY;
+}
+
+function collisionDetect(player, ball) {
+  player.top = player.y;
+  player.right = player.x + player.width;
+  player.bottom = player.y + player.height;
+  player.left = player.x;
+
+  ball.top = ball.y - ball.radius;
+  ball.right = ball.x + ball.radius;
+  ball.bottom = ball.y + ball.radius;
+  ball.left = ball.x - ball.radius;
+
+  return ball.left < player.right && ball.top < player.bottom && ball.right > player.left && ball.bottom > player.top;
+}
+
+function update() {
+  if (upArrowPressed && user.y > 0) {
+    user.y -= 8;
+  } else if (downArrowPressed && (user.y < canvas.height - user.height)) {
+    user.y += 8;
+  }
+  // check if ball hits top or bottom wall
+  if (ball.y + ball.radius >= canvas.height || ball.y - ball.radius <= 0) {
+    wallHitSound.play();
+    ball.velocityY = -ball.velocityY;
+  }
+  if (ball.x + ball.radius >= canvas.width) {
+    scoreSound.play();
+    user.score += 1;
+    reset();
+  }
+  if (ball.x - ball.radius <= 0) {
+    scoreSound.play();
+    ai.score += 1;
+    reset();
+  }
+
+  // move the ball
+  ball.x += ball.velocityX;
+  ball.y += ball.velocityY;
+
+  // ai paddle movement
+  ai.y += ((ball.y - (ai.y + ai.height / 2))) * 0.09;
+
+  // collision detection on paddles
+  let player = (ball.x < canvas.width / 2) ? user : ai;
+
+  if (collisionDetect(player, ball)) {
+    hitSound.play();
+    let angle = 0;
+
+    if (ball.y < (player.y + player.height / 2)) {
+      angle = -1 * Math.PI / 4;
+    } else if (ball.y > (player.y + player.height / 2)) {
+      angle = Math.PI / 4;
+    }
+    ball.velocityX = (player === user ? 1 : -1) * ball.speed * Math.cos(angle);
+    ball.velocityY = ball.speed * Math.sin(angle);
+
+    ball.speed += 0.2;
+  }
+}
+
+function render() {
+  ctx.fillStyle = '#000';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  drawNet();
+  drawScore(canvas.width / 4, canvas.height / 6, user.score);
+  drawScore((3 * canvas.width) / 4, canvas.height / 6, ai.score);
+  drawPaddle(user.x, user.y, user.width, user.height, user.color);
+  drawPaddle(ai.x, ai.y, ai.width, ai.height, ai.color);
+  drawBall(ball.x, ball.y, ball.radius, ball.color);
+};
+
+function gameLoop() {
+  update();
+  render();
+}
+
+setInterval(gameLoop, 1000 / 60);
